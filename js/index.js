faa550ffeaf4eb6b387fbb74e1eac436d491f726
Jessie <isai@protonmail.com>
2017-02-12 19:15:28 +0100

    Basiv response from api is working

--- js/index.js
+++ js/index.js
@@ -2,10 +2,20 @@
 
 $(document).ready(function() {
 
+    var API_URL = "https://en.wikipedia.org/w/api.php?action=opensearch&search=result&formt=json&limit=3"
+
   $("#search-bar-input").focus();
   $("#search-bar-input").on("input", function(){
     $("#search-bar-container").css("top", "10%");
-  })
+  });
+
+  $.get(API_URL, callback, "jsonp");
+
+
+  function callback(data){
+    console.log("response: "+JSON.stringify(data));
+  }
+
 
 
 });
