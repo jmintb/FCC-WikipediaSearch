256f3ab9cee98da574ecba1430bd0ba0abb73990
Jessie <isai@protonmail.com>
2017-02-14 01:27:48 +0100

    Alle the user stories have been met.

--- js/index.js
+++ js/index.js
@@ -1,28 +1,37 @@
 $(document).ready(function() {
 
-    function getAPI_URL(query){
-      return "https://en.wikipedia.org/w/api.php?action=opensearch&search="+query+"&formt=json&limit=50"
+    function getAPI_URL(query) {
+        return "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + query + "&formt=json&limit=50";
     }
+
+    var API_RANDOM_PAGE = "https://en.wikipedia.org/wiki/Special:Random";
     var searchResults = [];
-
-
+    var latestQuery;
+    var queryChanged = false;
 
     $("#search-bar-input").focus();
     $("#search-bar-input").on("input", function() {
-        $("#search-bar-container").css("top", "10%");
         var query = $("#search-bar-input").val();
-        console.log(!!query);
-        if(!!query){
-          $.get(getAPI_URL(query), resultHandler, "jsonp");
-        }else{
-          $("#result-list").empty();
-
-      }
+        latestQuery = query;
+        if (!!query) {
+          $("#load-icon").addClass("spinner");
+          $("#load-icon").removeClass("hide");
+            $.get(getAPI_URL(query), resultHandler, "jsonp");
+        } else {
+            $("#result-list").empty();
+            searchResults.length = 0;
+        }
 
     });
 
+    $("#random-btn").focus(function() {
+        $("#random-btn").blur();
+    });
 
 
+    function resultHandlerRndArticle(data){
+        console.log(JSON.stringify(data));
+    }
 
     function resultHandler(data) {
         for (var i = 0; i < data[1].length; i++) {
@@ -31,16 +40,27 @@ $(document).ready(function() {
             var link = data[3][i];
             searchResults.push(createSearchResult(title, description, link));
         }
+        if (data[0] != latestQuery) {
+            searchResults.length = 0;
+        } else if (!!latestQuery && data[0] == latestQuery) {
+            $("#result-list").empty();
+            $("#result-list").append(searchResults);
+            searchResults.length = 0;
+        } else {
+            $("#result-list").empty();
+            searchResults.length = 0;
 
-        $("#result-list").empty();
-        $("#result-list").append(searchResults);
-        searchResults.length = 0;
+        }
+        $("#load-icon").removeClass("spinner");
+        $("#load-icon").addClass("hide");
+
     }
 
     function createSearchResult(title, description, link) {
-        var innerHtml = '  <div class="panel-heading">'+title + '</div> <div class="panel-body">' + description + '</div>';
+        console.log("link: "+link);
+        var innerHtml = '<a class="search-result-li" href="'+link+'">  <div class="panel-heading">' + title + '</div> <div class="panel-body">' + description + '</div> </a>';
         var li = document.createElement("li");
-        $(li).addClass("search-result-div panel panel-primary");
+        $(li).addClass("search-result-li panel panel-primary");
         $(li).html(innerHtml);
         return li;
     }
